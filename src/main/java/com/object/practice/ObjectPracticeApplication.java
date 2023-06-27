fb5eed0a1c6dda77f4627c220b7ad4bde653d842
pursue503 <pursue503@naver.com>
2023-06-28 02:08:40 +0900

    "first commit"

---
+++ src/main/java/com/object/practice/ObjectPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.object.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/object/practice/ObjectPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.object.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
